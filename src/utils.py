4670fb19417ab52c58cac4660f7627a8dbf9235d
denanmax <deshis93@gmail.com>
2023-07-15 04:14:40 +0200

    improved / corrected

--- src/utils.py
+++ src/utils.py
@@ -28,11 +28,12 @@ def create_database(database_name, params):
                         CREATE TABLE IF NOT EXISTS vacancies (
                         id SERIAL PRIMARY KEY,
                         name TEXT NOT NULL,
-                        company_id INT REFERENCES companies(id),
+                        company_id INT,
                         salary_min INT,
                         salary_max INT,
-                        url TEXT NOT NULL
-                        );
+                        url TEXT NOT NULL,
+                        FOREIGN KEY (company_id) REFERENCES companies(id)
+    );
                     """)
 
     conn.commit()
@@ -46,23 +47,28 @@ def save_to_database(database_name, data, params):
         """Заполняем таблицу"""
         for vacancy in data:
             cur.execute("""
-                                            INSERT INTO companies(id, name, url)
-                                            VALUES (%s, %s, %s)
-                                            ON CONFLICT DO NOTHING
-                                         """,
-                        (vacancy['employer']['id'], vacancy['employer']['name'], vacancy['employer']['alternate_url']))
+                SELECT * FROM companies WHERE id = %s;
+            """, (vacancy['employer']['id'],))
+            existing_company = cur.fetchone()
+
+            if not existing_company:
+                cur.execute("""
+                    INSERT INTO companies(id, name, url)
+                    VALUES (%s, %s, %s);
+                """, (vacancy['employer']['id'], vacancy['employer']['name'],
+                      vacancy['employer']['alternate_url']))
 
         for vacancy in data:
             salary_from = salary_to = None
             if vacancy['salary']:
                 salary_from = vacancy['salary']['from']
                 salary_to = vacancy['salary']['to']
+
             cur.execute("""
-                 INSERT INTO vacancies (name, company_id, salary_min, salary_max, url)
-                 VALUES (%s, %s, %s, %s, %s)
-                 """,
-                        (vacancy['name'], vacancy['employer']['id'], salary_from, salary_to,
-                         vacancy['alternate_url']))
+                INSERT INTO vacancies (name, company_id,salary_min, salary_max, url)
+                VALUES (%s, %s, %s, %s, %s);
+            """, (vacancy['name'], vacancy['employer']['id'],salary_from, salary_to,
+                  vacancy['alternate_url']))
 
         conn.commit()
         conn.close()
